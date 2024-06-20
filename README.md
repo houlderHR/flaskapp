f79b87334eb02520a3e95845927f82ff13616aeb
houlderHR <houlder@hairun-technology.com>
2024-06-20 10:14:21 +0300

    sample code for student in formation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# flaskapp
+# Flask app sample project
+
+## Link reference
+
+- [Repository github sample collection](https://github.com/bradtraversy/50projects50days/tree/master)
+- [Tutorial Flask digital Ocean](https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3)
+- [Official website Flask](https://flask.palletsprojects.com/en/3.0.x/quickstart/)
---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_session import Session
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+load_dotenv()
+
+MYNAME = os.getenv('MYNAME')
+USEREMAIL = os.getenv('USEREMAIL')
+USERPASSWORD = os.getenv('USERPASSWORD')
+
+@app.route('/')
+@app.route('/home')
+def home():
+    context = None
+    if session.get("infosession"):
+        context = session['infosession']
+    return render_template('home/homepage.html', context={
+        'session':context,
+        'user':MYNAME
+    })
+
+
+@app.route('/rank')
+def rank():
+    context = None
+    if session.get("infosession"):
+        context = session['infosession']
+    return render_template('rankers/ranks.html', context={
+        'session':context,
+        'user':MYNAME
+    })
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email, pwd = request.form['email'], request.form['pwd']
+        if email == USEREMAIL and pwd == USERPASSWORD:
+            session['infosession'] = f"{email}_{pwd}"
+            return redirect(url_for('home'))
+    return render_template('account/signup.html')
+
+
+@app.route('/logout')
+def logout():
+    if session.get("infosession"):
+        session.pop('infosession', None)
+    return redirect(url_for('home'))
